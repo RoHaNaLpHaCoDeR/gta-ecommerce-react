bff5841a2087a82df0f94c42575518de2b61984a
RoHaNaLpHaCoDeR <rohanaroraaashi@gmail.com>
2021-05-16 03:47:08 +0530

    first commit

--- src/components/ProductPages/Cart/PayPalButton.js
+++ src/components/ProductPages/Cart/PayPalButton.js
@@ -31,7 +31,7 @@ export default class MyApp extends React.Component {
     // Document on Paypal's currency code: https://developer.paypal.com/docs/classic/api/currency_codes/
 
     const client = {
-      sandbox: process.env.REACT_APP_APP_ID,
+      sandbox: "AfDdJtHHgh0tuT7HlHZ3c9kraub_6htE8Xce8rzE_pXFjFBey384D4WRrYTSnK1x2gbn26NAUFaCF1r-",
       production: "YOUR-PRODUCTION-APP-ID"
     };
     // In order to get production's app-ID, you will have to send your app to Paypal for approval first
